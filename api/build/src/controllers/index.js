2a8666d95a7da3828f083dba347337689d5f93c4
Bruno David <93402677+DavidLBruno@users.noreply.github.com>
2022-08-14 01:58:47 -0300

    Delete api/build directory

--- api/build/src/controllers/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const mail_1 = require("../services/mail");
-const router = (0, express_1.Router)();
-router.post('/', mail_1.recibirMail);
-exports.default = router;
