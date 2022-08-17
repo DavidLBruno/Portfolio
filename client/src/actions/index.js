bc970ac9cff724a00067e43c909e07b59298d058
DavidLBruno <bruno.david9914@gmail.com>
2022-08-17 19:30:51 -0300

    Update

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 let { REACT_APP_URL } = process.env;
-
+/* Conect back */
 export const sendMail = async (body) => {
   try {
     const response = await axios.post(REACT_APP_URL, body);
